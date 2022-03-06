948cdb83b0e734a4ac7a22199e0ee5c15700213c
Hubert Muller <hubert@sportport.eu>
2022-03-06 13:53:23 +0100

    nowy komponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NaglowekComponent } from './naglowek/naglowek.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaglowekComponent
   ],
   imports: [
     BrowserModule,
