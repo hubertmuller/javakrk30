6d358b2567ab28bb85a89a7b754e65587884f4d5
Hubert Muller <hubert@sportport.eu>
2022-03-06 15:09:34 +0100

    nowe komponenty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NaglowekComponent } from './naglowek/naglowek.component';
 import { StopkaComponent } from './stopka/stopka.component';
+import { DodajComponent } from './dodaj/dodaj.component';
+import { ListaComponent } from './lista/lista.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NaglowekComponent,
-    StopkaComponent
+    StopkaComponent,
+    DodajComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
