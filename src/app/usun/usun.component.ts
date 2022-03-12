4756b6c2ce322f2b4af87695c2f7f32124c8e679
Hubert Muller <hubert@sportport.eu>
2022-03-12 15:11:24 +0100

    usuwnaie

--- src/app/usun/usun.component.ts
+++ src/app/usun/usun.component.ts
@@ -9,15 +9,24 @@ import { ListaService } from '../lista.service';
 })
 export class UsunComponent implements OnInit {
 
+  public zaladowano = false;
+  public blad = false;
+  public toDelete = 0;
+
   constructor(private route: ActivatedRoute, private listaService: ListaService) { 
 
     this.route.params.subscribe( (params) => {
-      const toDelete = params['id'];
+      this.toDelete = params['id'];
       //servis Lista i jego metoda delete
-      console.log('chcemy usunac id=' + toDelete);
-      this.listaService.deleteCzlowiek(toDelete).subscribe( 
+      console.log('chcemy usunac id=' + this.toDelete);
+      this.listaService.deleteCzlowiek(this.toDelete).subscribe( 
         (_) => {
-          console.log('udalo sie usunac id=' + toDelete);
+          console.log('udalo sie usunac id=' + this.toDelete);
+          this.zaladowano = true;
+        },
+        (_) => {
+          console.log('blad przy usuwaniu');
+          this.blad = true;
         }
       );
     });
