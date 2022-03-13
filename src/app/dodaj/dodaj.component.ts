d828b0b43ca9af26b9870ce17ff1df1b024b7a67
Hubert Muller <hubert@sportport.eu>
2022-03-13 09:31:29 +0100

    reactive formas dodaj komponent

--- src/app/dodaj/dodaj.component.ts
+++ src/app/dodaj/dodaj.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ListaService } from '../lista.service';
 
 @Component({
   selector: 'app-dodaj',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DodajComponent implements OnInit {
 
-  constructor() { }
+  constructor(private listaService: ListaService) { }
 
   ngOnInit(): void {
   }
