46f7c69d60d8518ca9687edfd8d312e7675b1945
Hubert Muller <hubert@sportport.eu>
2022-03-13 09:52:00 +0100

    form group

--- src/app/dodaj/dodaj.component.ts
+++ src/app/dodaj/dodaj.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ListaService } from '../lista.service';
 
 @Component({
@@ -7,6 +8,17 @@ import { ListaService } from '../lista.service';
   styleUrls: ['./dodaj.component.scss']
 })
 export class DodajComponent implements OnInit {
+
+  public forma: FormGroup = new FormGroup({
+    imie: new FormControl('', {
+      validators: [Validators.minLength(2)],
+      updateOn: 'change'
+    }),
+    nazwisko: new FormControl('', {
+      validators: [Validators.minLength(3)],
+      updateOn: 'change'
+    })
+  });
 
   constructor(private listaService: ListaService) { }
 
